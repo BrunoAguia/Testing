e6dc6186de596aba806407f555ba0c6737ed70cb
guil-hanma <guilherme.bezerra170@hotmail.com>
2024-11-29 16:46:53 -0300

    chore: Refactor guessing game code for better user experience

--- Jogo_Chute.py
+++ Jogo_Chute.py
@@ -1,6 +1,6 @@
 import random
 
-valor_aleatorio = random.randint(1,10)
+valor_aleatorio = random.randint(1, 10)
 acertou = False
 
 # Você sabe o que o código/jogo faz. O cliente/Jogador não, então sempre bom deixar claro nessas situações
@@ -20,7 +20,8 @@ while not acertou:
             print('Chutou Baixo')
         else:
             acertou = True
-            print('Parabéns, você acertou!')
+            # Isso que usei é uma FString, uma boa e dinâmica formatação de strings
+            print(f'Parabéns, você acertou! O número secreto era {valor_aleatorio}.')
     except ValueError:
         # Captura inputs que não são números inteiros
         print("Entrada inválida! Por favor, digite um número inteiro entre 1 e 10.")
